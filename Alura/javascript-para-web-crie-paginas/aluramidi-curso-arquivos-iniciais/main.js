597c2c6786f7eed2fef01e5a49db4034d8810500
Raissa <raissakestring23@gmail.com>
2024-04-09 18:19:46 -0300

    while, function, querySelector

---
+++ Alura/javascript-para-web-crie-paginas/aluramidi-curso-arquivos-iniciais/main.js
@@ -0,0 +1,17 @@
+function tocaSom(idElementoAudio) {
+  document.querySelector(idElementoAudio).play();
+}
+
+const ListaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+while (contador < ListaDeTeclas.length) {
+  ListaDeTeclas[contador].onclick = function() {
+    tocaSom('#som_tecla_pom');
+  }
+  
+  contador = contador + 1;
+ 
+  console.log(contador);
+}
