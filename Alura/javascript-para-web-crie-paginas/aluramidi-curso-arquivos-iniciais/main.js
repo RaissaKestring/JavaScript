5e89d033f251c4a434087ff407c33405111e7c68
Raissa <raissakestring23@gmail.com>
2024-04-10 18:16:36 -0300

    Novo curso JavaScript

--- Alura/javascript-para-web-crie-paginas/aluramidi-curso-arquivos-iniciais/main.js
+++ Alura/javascript-para-web-crie-paginas/aluramidi-curso-arquivos-iniciais/main.js
@@ -1,5 +1,13 @@
-function tocaSom(idElementoAudio) {
-  document.querySelector(idElementoAudio).play();
+function tocaSom(seletorAudio) {
+  const elemento = document.querySelector(seletorAudio);
+
+  if(elemento != null && elemento.localName === 'audio') {
+        elemento.play();
+  }
+  else {
+     //alert('Elemento nao encontrado');
+    console.log('Elemento nao encontrado ou seletor invalido');
+  }
 }
 
 const ListaDeTeclas = document.querySelectorAll('.tecla');
@@ -14,8 +22,17 @@ for (let contador = 0; contador < ListaDeTeclas.length; contador ++) { //for per
   tecla.onclick = function() {
     tocaSom(idAudio);
   }
-}
-  
+
+  tecla.onkeydown = function (evento) {
+    if (evento.code === 'Space' || evento.code === 'Enter') { // inves de criar varios if, uso || (ou)
+      tecla.classList.add('ativa');
+    }
+  }
+
+  tecla.onkeyup = function() {
+    tecla.classList.remove('ativa');
+  }
+}  
   /*
   contador = contador + 1;
   mesma coisa que: 
--- Alura/javascript-para-web-crie-paginas/teste.js
+++ Alura/javascript-para-web-crie-paginas/teste.js
@@ -1,6 +1,11 @@
 // atividade 1) Incrementando o contador no console
 
-for (let contador = 0 && contador >=10) {
-  console.log(document.querySelector('.botao'));
-  botao.onclick = contador + 1;
+let contador = 0;
+
+function incrementarContador() {
+  contador = contador + 1;
+  console.log('Contador: ' + contador);
 }
+
+const botao = document.querySelector('.botao');
+botao.onclick = incrementarContador;
